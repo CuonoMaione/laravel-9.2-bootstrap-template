d58b0e5c57b9c76e2052e0f44b81c98b0ebec131
CuonoMaione <cuonomaione99@outlook.it>
2023-07-24 03:26:00 +0200

    fix errore section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,16 @@
 
 @section('title', 'Welcome')
 
-@section('main-section')
+@section('main-content')
 
-    <h1>
-        Helloword!
-    </h1>
+    <h1>Helloword!</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 btn btn-danger">
+                    ciao
+                </div>
+            </div>
+        </div>
+    
 
 @endsection
